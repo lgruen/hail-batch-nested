c78b7a0d7627000ac1c3eee54610cbfc7102262d
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2020-12-21 17:07:08 +1100

    Fix file checks.

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 docker:
-	docker build -t gcr.io/${GCP_PROJECT}/hail-sub-batch:latest .
-	docker push gcr.io/${GCP_PROJECT}/hail-sub-batch:latest
+	docker build -t gcr.io/${GCP_PROJECT}/hail-batch-nested:latest .
+	docker push gcr.io/${GCP_PROJECT}/hail-batch-nested:latest
--- inner.py
+++ inner.py
@@ -6,16 +6,7 @@ backend = hb.ServiceBackend(
 
 b = hb.Batch(backend=backend, name='inner') 
 
-j1 = b.new_job(name='hello') 
-j1.command('echo "hello world"') 
-
-j2 = b.new_job(name='check gsa') 
-j2.command('ls -l /gsa-key/key.json') 
-
-j3 = b.new_job(name='check user-token') 
-j3.command('ls -l /user-tokens/tokens.json') 
-
-j4 = b.new_job(name='check deploy-config') 
-j4.command('ls -l /deploy-config/deploy-config.json') 
+j = b.new_job(name='hello') 
+j.command('echo "hello world"') 
 
 b.run()
